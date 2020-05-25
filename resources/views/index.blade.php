d050cf343eecc6c42c563d8f86b5897a1a61c47e
Shadowkibo <amitmojumder356@gmail.com>
2020-05-25 20:08:43 +0600

    React SetUp

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}" />
+</head>
+
+<div id="root"></div>
+<script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+</body>
+
+</html>
